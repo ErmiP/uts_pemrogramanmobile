54fe93d8b1061d6f2c25b3c59af0e858f9360666
ErmiP <ermi.pristiya@gmail.com>
2019-10-17 13:24:39 +0700

    uts

---
+++ app/src/main/java/id/ac/polinema/uts_android/MainActivity.java
@@ -0,0 +1,21 @@
+package id.ac.polinema.uts_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void siswaHandler(View view) {
+        Intent intent = new Intent(this, SiswaActivity.class);
+        startActivity(intent);
+    }
+}
